c308ecf6d24e3bf12de076abf57286a64f2de650
Bradey Lounsbury <bl396918@ohio.edu>
2022-07-30 09:14:34 -0400

    trying diff intent format

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
 
-const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
+const client = new Discord.Client({intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+]});
 
 //messages that start with this trigger bot
 const prefix = '-';
