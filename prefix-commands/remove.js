c597c0ee235ae9093107c979c39ee879d10ebc6c
Bradey Lounsbury <bl396918@ohio.edu>
2022-08-13 12:51:02 -0400

    added remove command

--- prefix-commands/remove.js
+++ prefix-commands/remove.js
@@ -1,22 +1,30 @@
 const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
-    name: 'queue',
-    aliases: 'q',
-    description: 'shows songs queued up    **Alias:**  *-q*',
+    name: 'remove',
+    aliases: 'rl',
+    description: 'removes song at given place in queue *(aliases remove last song)*    **Aliases:**  *-rl* *-remove-last*',
     // eslint-disable-next-line no-unused-vars
     execute: async ({ client, commandName, message }) => {
+        console.log(commandName);
         const queue = client.player.getQueue(message.guildId);
-        if (!queue || !queue.playing) return await message.channel.send('No songs queued');
+        if (!queue || !queue.playing) return await message.channel.send('No song to remove :(');
 
-        let queueLength = 10;
-        if (queue.tracks.length < queueLength) queueLength = queue.tracks.length;
+        const queueLength = queue.tracks.length;
+        if (queueLength === 0) return await message.channel.send('No song to remove :(');
 
+        if (commandName === 'rl' || commandName === 'remove-last') {
+            queue.tracks.pop();
+            console.log('removed %d', queueLength);
+        }
+        else {
+            const index = parseInt(message.content);
+            queue.tracks.splice(index);
+            console.log('removed %d', index);
+        }
         const queueString = queue.tracks.slice(0, queueLength).map((song, i) => {
             return `**${ i + 1 } \`[${song.duration}]\` ${song.title} -- <@${song.requestedBy.id}>**`;
         }).join('\n');
-
-        console.log(queueString);
 
         const currentSong = queue.current;
 
@@ -26,6 +34,7 @@ module.exports = {
             // eslint-disable-next-line quotes
             .setDescription(`**Currently Playing**\n` + (currentSong ? `\`[${currentSong.duration}]\` ${currentSong.title} -- <@${currentSong.requestedBy.id}>` : 'None') + `\n\n**Queue**\n${queueString}`)
             .setThumbnail(currentSong.setThumbnail);
+        await message.channel.send('Here\'s the updated queue');
         await message.channel.send({ embeds: [embed] });
     },
 };
